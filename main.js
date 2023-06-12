e7e0e089dcf7eafd8547e4fcbe0c7c2499701eff
Łukasz <lukaszkogut1996@gmail.com>
2023-06-12 20:52:07 +0200

    first hw

---
+++ main.js
@@ -0,0 +1,19 @@
+let myVariable = 10;
+let myName = "Benedykt"
+
+myVariable++
+
+console.log(myVariable);
+
+myName = "Mieszko"
+
+console.log(myName);
+
+
+let sum = ["myVariable", 5]
+
+console.log(sum);
+
+let greeting = [myName,"witaj w świecie JavaScript!"]
+
+console.log(greeting);
