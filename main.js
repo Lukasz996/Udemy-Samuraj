c1040e51328197af992d14811c985eee80c19009
Łukasz <lukaszkogut1996@gmail.com>
2024-04-14 20:31:56 +0200

    +

--- main.js
+++ main.js
@@ -1,19 +1,44 @@
-let myVariable = 10;
-let myName = "Benedykt"
-
-myVariable++
-
-console.log(myVariable);
-
-myName = "Mieszko"
-
-console.log(myName);
 
 
-let sum = ["myVariable", 5]
+// for ( let i = 0; i < 10; i++) {
+// 	console.log("wyświetlenie " + i);
+// }
 
-console.log(sum);
+// const showMyName = function (userName) {
+// 	console.log(userName);
+   
+// }
 
-let greeting = [myName,"witaj w świecie JavaScript!"]
+// showMyName('')
 
-console.log(greeting);
+
+// const numbers = number => {
+// 	console.log(number / 2)
+// }
+
+// const sum = numbers(15)
+
+// let number = 0
+// const add = function() {
+// 	number++
+//     console.log(add);
+// }
+
+// let myVariable = 10;
+// let myName = "Benedykt"
+
+// myVariable++
+
+// console.log(myVariable);
+
+// myName = "Mieszko"
+
+// console.log(myName);
+
+// let sum = ["myVariable", 5]
+
+// console.log(sum);
+
+// let greeting = [myName,"witaj w świecie JavaScript!"]
+
+// console.log(greeting);
