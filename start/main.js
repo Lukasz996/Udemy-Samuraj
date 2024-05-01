6c29c1496d5ac2061f37fbb4e9795436ded9bc85
Łukasz <63703626+Lukasz996@users.noreply.github.com>
2024-05-01 18:21:35 +0200

    learn

--- start/main.js
+++ start/main.js
@@ -1,2 +1,14 @@
-const userName = "Arek";
-console.log("hello " + userName);
+const userName = "Bolesław";
+const userAge = 22;
+const bossFamilly = false;
+
+if ((userName != "" && typeof userName ==="string") || bossFamilly) {
+	console.log("Witaj, " + userName +"!");
+	if (userAge <= 17) {
+		console.log("Masz " + userAge + " lat, nie możesz wejść");
+	} else {
+		console.log("Masz " + userAge + ", zapraszamy!");
+	}
+} else {
+	console.log("Nie możesz wejść, nie wiem jak się nazywasz!");
+}
