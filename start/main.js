6f327a9f7fa5f1909992541da579b931105acc91
Łukasz <63703626+Lukasz996@users.noreply.github.com>
2024-05-01 20:02:38 +0200

    main.js

--- start/main.js
+++ start/main.js
@@ -1,14 +1,17 @@
-const userName = "Bolesław";
-const userAge = 22;
-const bossFamilly = false;
+// const userName = "Bolesław";
+// const userAge = 22;
+// const bossFamilly = false;
 
-if ((userName != "" && typeof userName ==="string") || bossFamilly) {
-	console.log("Witaj, " + userName +"!");
-	if (userAge <= 17) {
-		console.log("Masz " + userAge + " lat, nie możesz wejść");
-	} else {
-		console.log("Masz " + userAge + ", zapraszamy!");
-	}
-} else {
-	console.log("Nie możesz wejść, nie wiem jak się nazywasz!");
-}
+// if ((userName != "" && typeof userName ==="string") || bossFamilly) {
+// 	console.log("Witaj, " + userName +"!");
+// 	if (userAge <= 17) {
+// 		console.log("Masz " + userAge + " lat, nie możesz wejść");
+// 	} else {
+// 		console.log("Masz " + userAge + ", zapraszamy!");
+// 	}
+// } else {
+// 	console.log("Nie możesz wejść, nie wiem jak się nazywasz!");
+// }
+
+const playerName = "Kratos";
+console.log(`Witaj ${playerName ? playerName + "!" : "nieznajomy"}`);
