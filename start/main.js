738becb175a0ffde634563ebbe284fd13660e4ea
Łukasz <63703626+Lukasz996@users.noreply.github.com>
2024-05-05 19:15:13 +0200

    +

--- start/main.js
+++ start/main.js
@@ -18,12 +18,50 @@
 
 // const game = ([time, points, name] = [123.123, 15, "Uti"]);
 
-const animals = ["dog", "cat", "canary", "mouse"];
-// animals.splice(0, 3);
+// const animals = ["dog", "cat", "canary", "mouse"];
+// // animals.splice(0, 3);
 
-const namesUsers = ["Zenon", "Anna", "Jacek", "Lukasz", "Gabriel"];
-namesUsers.sort()
-console.log(namesUsers);
+// const namesUsers = ["Zenon", "Anna", "Jacek", "Lukasz", "Gabriel"];
+// namesUsers.sort()
+// console.log(namesUsers);
 
-namesUsers.splice(1, 2);
-console.log(namesUsers);
+// namesUsers.splice(1, 2);
+// console.log(namesUsers);
+
+// const numbers = [5, 10, 15];
+
+// if (numbers[0] + numbers[2] > numbers[1]) {
+// 	console.log(
+// 		"Suma pierwszego i ostatniego elementu jest większa niż drugi element"
+// 	);
+// } else {
+// 	console.log(
+// 		"Suma pierwszego i ostatniego elementu nie jest większa niż drugi element"
+// 	);
+// }
+
+// const firstArray = [1, 2, 3];
+// const secondArray = [4, 5];
+
+// if (firstArray.length > secondArray.length) {
+// 	console.log("Pierwsza tablica jest dłuższa");
+// } else if (secondArray.length > firstArray.length) {
+// 	console.log("Druga tablica jest dłuższa");
+// } else {
+// 	console.log("Tablice są takie same");
+// }
+
+// const fruits_v1 = ["pear", "banana", "orange"];
+// const fruits_v2 = ["apple", "banana", "orange"];
+
+// if (fruits_v1[0] === "apple") {
+// 	console.log("Pierwszy element tablicy to 'apple'");
+// } else {
+// 	console.log("Pierwszy element tablicy to nie 'apple");
+// }
+// if (fruits_v2[0] === "apple") {
+// 	console.log("Pierwszy element tablicy to 'apple'");
+// } else {
+// 	console.log("Pierwszy element tablicy to nie 'apple");
+// }
+
