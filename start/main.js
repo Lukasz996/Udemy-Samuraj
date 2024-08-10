f3f77463bca99b54da8e5cac689b047935db819c
Łukasz <63703626+Lukasz996@users.noreply.github.com>
2024-08-10 21:14:44 +0200

    growing square

--- start/main.js
+++ start/main.js
@@ -1,5 +1,27 @@
+const squere = document.createElement("div");
+document.body.appendChild(squere);
 
+let grow = true;
 
+let size = 100;
+squere.style.width = size + "px";
+
+window.addEventListener("scroll", function () {
+	if (size >= window.innerWidth / 2) {
+		grow = false;
+	} else if (size <= 0) {
+		grow = true;
+	}
+	if (grow == true) {
+		size += 5;
+		squere.style.width = size + "px";
+		squere.style.height = size + "px";
+	} else {
+		size -= 5;
+		squere.style.width = size + "px";
+		squere.style.height = size + "px";
+	}
+});
 
 // ==========================================================
 
