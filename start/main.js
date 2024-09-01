642ea21e7c773dacc0372a8b9f7f58ecac8c83a6
Łukasz <63703626+Lukasz996@users.noreply.github.com>
2024-09-01 18:15:22 +0200

    changing background color by moving the mouse

--- start/main.js
+++ start/main.js
@@ -1,7 +1,16 @@
 document.body.addEventListener("mousemove", (e) => {
 	console.log("ruch");
-	document.querySelector('h1').textContent = `${e.clientX},${e.clientY}`
-})
+	document.querySelector("h1").textContent = `${e.clientX},${e.clientY}`;
+
+	const x = e.clientX;
+	const y = e.clientY;
+	const width = window.innerWidth;
+	const height = window.innerHeight;
+	const red = (x / width) * 100;
+	const green = (y / height) * 100;
+	const blue = ((x / width) * 100 + (y / height) * 100) / 2;
+	document.body.style.backgroundColor = `rgb(${red}%,${green}%,${blue}%)`;
+});
 
 // ================================================================
 
