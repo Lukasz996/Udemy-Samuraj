9edcdfdf267d9dbc8191b19554a85ffe2837e4a8
Łukasz <63703626+Lukasz996@users.noreply.github.com>
2024-09-16 18:36:45 +0200

    v2 switch

--- start/main.js
+++ start/main.js
@@ -21,13 +21,18 @@ document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 const changeColor = (e) => {
 	switch (e.keyCode) {
 		case 38:
-			document.body.style.backgroundColor = `rgb(${(red += 5)}, ${(green += 5)}, ${(blue += 5)})`;
+			document.body.style.backgroundColor = `rgb(${
+				red <= 255 ? (red += 5) : red
+			}, ${green <= 255 ? (green += 5) : green}, ${
+				blue <= 255 ? (blue += 5) : blue
+			})`;
 			break;
 		case 40:
-			document.body.style.backgroundColor = `rgb(${(red -= 5)}, ${(green -= 5)}, ${(blue -= 5)})`;
-			break;
-
-		default:
+			document.body.style.backgroundColor = `rgb(${
+				red > 0 ? (red -= 5) : red
+			}, ${green > 0 ? (green -= 5) : green}, ${
+				blue > 0 ? (blue -= 5) : blue
+			})`;
 			break;
 	}
 	console.log(40, red, green, blue);
